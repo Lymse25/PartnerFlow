faa394227c4705331aa937da7d3e30f78ed6b310
Lymse25 <lymae01@gmail.com>
2025-07-20 16:24:47 +0200

    Initial commit

---
+++ src/main/java/com/partnerflow/PartnerFlowApplication.java
@@ -0,0 +1,13 @@
+package com.partnerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartnerFlowApplication.class, args);
+    }
+
+}
